fdd3e38a4e2ad11eed0ff0d1070d6b57a49280af
enriqueaco <enriqueaco@gmail.com>
2019-12-04 12:42:35 -0500

    Versión incial
    Versión inicial

---
+++ models/db.py
@@ -0,0 +1,224 @@
+# -*- coding: utf-8 -*-
+
+# -------------------------------------------------------------------------
+# AppConfig configuration made easy. Look inside private/appconfig.ini
+# Auth is for authenticaiton and access control
+# -------------------------------------------------------------------------
+from gluon.contrib.appconfig import AppConfig
+from gluon.tools import Auth
+
+# -------------------------------------------------------------------------
+# This scaffolding model makes your app work on Google App Engine too
+# File is released under public domain and you can use without limitations
+# -------------------------------------------------------------------------
+
+if request.global_settings.web2py_version < "2.15.5":
+    raise HTTP(500, "Requires web2py 2.15.5 or newer")
+
+# -------------------------------------------------------------------------
+# if SSL/HTTPS is properly configured and you want all HTTP requests to
+# be redirected to HTTPS, uncomment the line below:
+# -------------------------------------------------------------------------
+# request.requires_https()
+
+# -------------------------------------------------------------------------
+# once in production, remove reload=True to gain full speed
+# -------------------------------------------------------------------------
+configuration = AppConfig(reload=True)
+
+if not request.env.web2py_runtime_gae:
+    # ---------------------------------------------------------------------
+    # if NOT running on Google App Engine use SQLite or other DB
+    # ---------------------------------------------------------------------
+    db = DAL(configuration.get('db.uri'),
+             pool_size=configuration.get('db.pool_size'),
+             migrate_enabled=configuration.get('db.migrate'),
+             check_reserved=['all'])
+else:
+    # ---------------------------------------------------------------------
+    # connect to Google BigTable (optional 'google:datastore://namespace')
+    # ---------------------------------------------------------------------
+    db = DAL('google:datastore+ndb')
+    # ---------------------------------------------------------------------
+    # store sessions and tickets there
+    # ---------------------------------------------------------------------
+    session.connect(request, response, db=db)
+    # ---------------------------------------------------------------------
+    # or store session in Memcache, Redis, etc.
+    # from gluon.contrib.memdb import MEMDB
+    # from google.appengine.api.memcache import Client
+    # session.connect(request, response, db = MEMDB(Client()))
+    # ---------------------------------------------------------------------
+
+# -------------------------------------------------------------------------
+# by default give a view/generic.extension to all actions from localhost
+# none otherwise. a pattern can be 'controller/function.extension'
+# -------------------------------------------------------------------------
+response.generic_patterns = [] 
+if request.is_local and not configuration.get('app.production'):
+    response.generic_patterns.append('*')
+
+# -------------------------------------------------------------------------
+# choose a style for forms
+# -------------------------------------------------------------------------
+response.formstyle = 'bootstrap4_inline'
+response.form_label_separator = ''
+
+# -------------------------------------------------------------------------
+# (optional) optimize handling of static files
+# -------------------------------------------------------------------------
+# response.optimize_css = 'concat,minify,inline'
+# response.optimize_js = 'concat,minify,inline'
+
+# -------------------------------------------------------------------------
+# (optional) static assets folder versioning
+# -------------------------------------------------------------------------
+# response.static_version = '0.0.0'
+
+# -------------------------------------------------------------------------
+# Here is sample code if you need for
+# - email capabilities
+# - authentication (registration, login, logout, ... )
+# - authorization (role based authorization)
+# - services (xml, csv, json, xmlrpc, jsonrpc, amf, rss)
+# - old style crud actions
+# (more options discussed in gluon/tools.py)
+# -------------------------------------------------------------------------
+
+# host names must be a list of allowed host names (glob syntax allowed)
+auth = Auth(db, host_names=configuration.get('host.names'))
+
+# -------------------------------------------------------------------------
+# create all tables needed by auth, maybe add a list of extra fields
+# -------------------------------------------------------------------------
+auth.settings.extra_fields['auth_user'] = []
+auth.define_tables(username=False, signature=False)
+
+# -------------------------------------------------------------------------
+# configure email
+# -------------------------------------------------------------------------
+mail = auth.settings.mailer
+mail.settings.server = 'logging' if request.is_local else configuration.get('smtp.server')
+mail.settings.sender = configuration.get('smtp.sender')
+mail.settings.login = configuration.get('smtp.login')
+mail.settings.tls = configuration.get('smtp.tls') or False
+mail.settings.ssl = configuration.get('smtp.ssl') or False
+
+# -------------------------------------------------------------------------
+# configure auth policy
+# -------------------------------------------------------------------------
+auth.settings.registration_requires_verification = True
+auth.settings.registration_requires_approval = True
+auth.settings.reset_password_requires_verification = True
+
+# -------------------------------------------------------------------------  
+# read more at http://dev.w3.org/html5/markup/meta.name.html               
+# -------------------------------------------------------------------------
+response.meta.author = configuration.get('app.author')
+response.meta.description = configuration.get('app.description')
+response.meta.keywords = configuration.get('app.keywords')
+response.meta.generator = configuration.get('app.generator')
+
+# -------------------------------------------------------------------------
+# your http://google.com/analytics id                                      
+# -------------------------------------------------------------------------
+response.google_analytics_id = configuration.get('google.analytics_id')
+
+# -------------------------------------------------------------------------
+# maybe use the scheduler
+# -------------------------------------------------------------------------
+if configuration.get('scheduler.enabled'):
+    from gluon.scheduler import Scheduler
+    scheduler = Scheduler(db, heartbeat=configure.get('heartbeat'))
+
+# -------------------------------------------------------------------------
+# Define your tables below (or better in another model file) for example
+#
+# >>> db.define_table('mytable', Field('myfield', 'string'))
+#
+# Fields can be 'string','text','password','integer','double','boolean'
+#       'date','time','datetime','blob','upload', 'reference TABLENAME'
+# There is an implicit 'id integer autoincrement' field
+# Consult manual for more options, validators, etc.
+#
+# More API examples for controllers:
+#
+# >>> db.mytable.insert(myfield='value')
+# >>> rows = db(db.mytable.myfield == 'value').select(db.mytable.ALL)
+# >>> for row in rows: print row.id, row.myfield
+# -------------------------------------------------------------------------
+
+# -------------------------------------------------------------------------
+# after defining tables, uncomment below to enable auditing
+# -------------------------------------------------------------------------
+# auth.enable_record_versioning(db
+db=DAL('sqlite://storage.sqlite')
+db.define_table("registroprestamos01",Field("indice","text",unique=True),
+                                            Field("tipoprestamo","text"),
+                                            Field("titulo0","text"),
+                                            Field("codigo0","text"),
+                                            Field("autor0","text"),
+                                            Field("volumen0","text"),
+                                            Field("numero0","text"),
+                                            Field("titulo1","text"),
+                                            Field("codigo1","text"),
+                                            Field("autor1","text"),
+                                            Field("volumen1","text"),
+                                             Field("numero1","text"),
+                                            Field("titulo2","text"),
+                                            Field("codigo2","text"),
+                                            Field("autor2","text"),
+                                            Field("volumen2","text"),
+                                             Field("numero2","text"),
+                                            Field("titulo3","text"),
+                                            Field("codigo3","text"),
+                                            Field("autor3","text"),
+                                            Field("volumen3","text"),
+                                             Field("numero3","text"),
+                                            Field("titulo4","text"),
+                                            Field("codigo4","text"),
+                                            Field("autor4","text"),
+                                            Field("volumen4","text"),
+                                             Field("numero4","text"),
+                                            Field("titulo5","text"),
+                                            Field("codigo5","text"),
+                                            Field("autor5","text"),
+                                            Field("volumen5","text"),
+                                             Field("numero5","text"),
+                                            Field("titulo6","text"),
+                                            Field("codigo6","text"),
+                                            Field("autor6","text"),
+                                            Field("volumen6","text"),
+                                             Field("numero6","text"),
+                                            Field("titulo7","text"),
+                                            Field("codigo7","text"),
+                                            Field("autor7","text"),
+                                            Field("volumen7","text"),
+                                             Field("numero7","text"),
+                                            Field("titulo8","text"),
+                                            Field("codigo8","text"),
+                                            Field("autor8","text"),
+                                            Field("volumen8","text"),
+                                             Field("numero8","text"),
+                                            Field("titulo9","text"),
+                                            Field("codigo9","text"),
+                                            Field("autor9","text"),
+                                            Field("volumen9","text"),
+                                             Field("numero9","text"),
+                                            Field("categoria","text"),
+                                            Field("especialidad","text"),
+                                            Field("nombre","text"),
+                                            Field("apellidos","text"),
+                                            Field("institucion","text"),
+                                            Field("ci","text"),
+                                            Field("email","text"),
+                                            Field("nota","text"),
+                                            Field("fechaprestamo","date",requires=IS_DATE('%d-%m-%Y')),
+                                            Field("fechadevolucion","date",requires=IS_DATE('%d-%m-%Y')),
+                                            Field("devuelto","integer"),
+                                            Field("biblioteca","text"),
+                                            Field("usuario","text"))
+
+from gluon.tools import Auth
+auth = Auth(db)
+auth.define_tables(username=True)
